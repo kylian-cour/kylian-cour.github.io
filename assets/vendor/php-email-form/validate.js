40c7d2e3778b99647925b9c03c7d76343428c32b
kylian-cour <kyliancourousse.pro@gmail.com>
2026-09-13 15:39:31 +0200

    Update EmailJS template ID in validate.js

--- assets/vendor/php-email-form/validate.js
+++ assets/vendor/php-email-form/validate.js
@@ -20,7 +20,7 @@
     errorMessage.classList.remove('d-block');
     sentMessage.classList.remove('d-block');
 
-    emailjs.send("service_oyigoax", "TEMPLATE_ID_ICI", {
+    emailjs.send("service_oyigoax", "template_4vx0fte", {
       name: form.name.value,
       email: form.email.value,
       subject: form.subject.value,
